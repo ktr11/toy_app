a091e7b94fd8bdc2a646067ebc5b973e2e01575f
ktr11 <kenmcz315@gmail.com>
2019-11-02 12:41:21 +0900

    add users and micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
+  resources :microposts
+  resources :users
     root 'application#hello'
   end
 end
